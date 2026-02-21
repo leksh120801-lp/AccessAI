f202d2ec4d89d0354e7363b2ebb5aab3b496c493
Rahaf Alharbi <s2825802@ed.ac.uk>
2026-02-21 00:09:06 +0000

    adding AI and more features

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ async function getActiveTab() {
   return tab;
 }
 
-const GEMINI_API_KEY = "AIzaSyCGJj5SPChskrhPiRdASErHqQ_wi_Qt6qI";
+const GEMINI_API_KEY = "GEMNI-API-KEY-HERE";
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 async function callGemini(prompt, retryCount = 0) {
